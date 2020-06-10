c361480ae9b47e206bc7a5835fbc31dbb1f0fe22
Ashwin Gairola <ashwingairola999@gmail.com>
2020-06-10 19:25:23 +0530

    Product converted to class.

--- 2-express-intro/src/models/Product.js
+++ 2-express-intro/src/models/Product.js
@@ -2,27 +2,32 @@ import Sequelize from 'sequelize';
 
 import { SEQUELIZE } from '../util/database.js';
 
-export const Product = SEQUELIZE.define('product', {
-	id: {
-		type: Sequelize.INTEGER,
-		autoIncrement: true,
-		allowNull: false,
-		primaryKey: true
+export class Product extends Sequelize.Model {}
+
+Product.init(
+	{
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			allowNull: false,
+			primaryKey: true
+		},
+		title: {
+			type: Sequelize.STRING,
+			allowNull: false
+		},
+		price: {
+			type: Sequelize.DOUBLE,
+			allowNull: false
+		},
+		imageUrl: {
+			type: Sequelize.STRING,
+			allowNull: false
+		},
+		description: {
+			type: Sequelize.TEXT,
+			allowNull: false
+		}
 	},
-	title: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	price: {
-		type: Sequelize.DOUBLE,
-		allowNull: false
-	},
-	imageUrl: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	description: {
-		type: Sequelize.TEXT,
-		allowNull: false
-	}
-});
+	{ sequelize: SEQUELIZE, modelName: 'product' }
+);
